7b61c525ecd3f461f1526a47fafc5653e10e657c
shreya <mishrashreya1105@gmail.com>
2025-03-04 11:24:00 +0530

    github data added

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 4000;
+const githubdata={
+  "login": "Shreya271",
+  "id": 145209480,
+  "node_id": "U_kgDOCKe4iA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/145209480?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Shreya271",
+  "html_url": "https://github.com/Shreya271",
+  "followers_url": "https://api.github.com/users/Shreya271/followers",
+  "following_url": "https://api.github.com/users/Shreya271/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Shreya271/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Shreya271/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Shreya271/subscriptions",
+  "organizations_url": "https://api.github.com/users/Shreya271/orgs",
+  "repos_url": "https://api.github.com/users/Shreya271/repos",
+  "events_url": "https://api.github.com/users/Shreya271/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Shreya271/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "SHREYA MISHRA",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 4,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 2,
+  "created_at": "2023-09-16T13:55:24Z",
+  "updated_at": "2025-03-04T03:52:45Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -12,13 +47,17 @@ app.get('/twitter', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.send('<h1>Please login at chai aur code</h1>'); // ✅ Fixed closing tag
+  res.send('<h1>Please login at chai aur code</h1>'); 
 });
 
 
 app.get('/youtube', (req, res) => {
-    res.send('<h2> chai aur code</h2>'); // ✅ Fixed closing tag
+    res.send('<h2> chai aur code</h2>'); 
   });
+app.get('/github', (req, res) => {
+    res.json(githubdata)
+  });
+
   
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on http://localhost:${port}`);
